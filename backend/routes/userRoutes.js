0157db156014e6843c4c5c91089d0dea75cf2cbe
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-11-29 13:43:32 +0530

    feat(backend): store s3 keys, return presigned URLs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,22 +1,35 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const {
+//   registerUser,
+//   loginUser,
+//   getUsers,
+//   getUserById,
+//   updateUser,
+//   deleteUser,
+//   deleteUserDebug
+// } = require('../controllers/userController');
+
+// // routes mapping
+// router.post('/register', registerUser);
+// router.post('/login', loginUser);
+// router.get('/', getUsers);
+// router.get('/:id', getUserById);
+// router.put('/:id', updateUser);
+// router.delete('/:id', deleteUserDebug);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-const {
-  registerUser,
-  loginUser,
-  getUsers,
-  getUserById,
-  updateUser,
-  deleteUser,
-  deleteUserDebug
-} = require('../controllers/userController');
-
-// routes mapping
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUserDebug);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id/debug', userController.deleteUserDebug); // debug version
 
 module.exports = router;
