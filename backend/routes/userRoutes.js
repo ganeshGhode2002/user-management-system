9ac98c49c97bcde2f43ada7c69f09e2bdced3b5b
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-12-02 14:09:34 +0530

    change to routs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -30,6 +30,6 @@ router.post('/login', userController.loginUser);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUserDebug); // debug version
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
