e53dad7a8d7526d4538998902c37dc17670a6970
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-12-02 12:24:45 +0530

    cleaning code

--- frontend/src/pages/Landing/LandingPage.jsx
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/Landing/LandingPage.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 
--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,4 +1,3 @@
-// src/pages/Profile/ProfilePage.jsx
 import React, { useEffect, useState, useContext, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import API, { SERVER_BASE } from "@/api/axios";
